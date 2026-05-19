6245bd55d890829f7ff350358e41baf19fa9cf75
Reiji <reqrup91@gmail.com>
2026-05-19 16:35:06 +0900

    fix:log追加

--- useLocalStorage.tsx
+++ useLocalStorage.tsx
@@ -1,18 +1,31 @@
 import { useEffect, useState } from "react";
+import type z from "zod";
 
-function useLocalStorage<T>(key: string, initialValue: T) {
+function useLocalStorage<T>(
+  key: string,
+  initialValue: T,
+  schema: z.ZodType<T>
+) {
   const [value, setValue] = useState<T>(() => {
     const value = localStorage.getItem(key);
     if (!value) {
       return initialValue;
     } else {
-      return JSON.parse(value);
+      const parseValue = JSON.parse(value);
+
+      const result = schema.safeParse(parseValue);
+
+      if (!result.success) {
+        return initialValue;
+      } else {
+        return result.data;
+      }
     }
   });
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
-  }, [value]);
+  }, [key, value]);
 
   return [value, setValue] as const;
 }
